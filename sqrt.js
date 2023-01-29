e94618ce5f298c46892151bc77d5e81737d7972d
DevRazib <ahmedrazib477@gmail.com>
2023-01-29 23:04:56 +0600

    js problem solving

---
+++ sqrt.js
@@ -0,0 +1,23 @@
+
+
+//take the input from the user
+//Math.sqrt(number);
+
+const number=64;
+const result=Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
+
+
+//square root different data type
+
+const num1=9;
+const num2=-25;
+const num3='Razib';
+
+const result1=Math.sqrt(num1)
+const result2=Math.sqrt(num2)
+const result3=Math.sqrt(num3)
+
+console.log(`The square root of ${num1} is ${result1}`)
+console.log(`The square root of ${num2} is ${result2}`)//In square root negative number and string is showed NaN
+console.log(`The square root of ${num3} is ${result3}`)//In square root negative number and string is showed NaN
---
+++ twoNumberSum.js
@@ -0,0 +1,24 @@
+
+
+function mySum(a,b){
+	return a + b;
+}
+const result=mySum(5,95);
+console.log(result);
+
+
+let a=38;
+let b=62;
+let sum=a+b;
+console.log(`sum of ${a} and ${b} is: ${sum}`)
+
+//store input numbers
+const num1=parseInt(prompt('Enter the first Number'))
+const num2=parseInt(prompt('Enter the second Number'))
+
+//add two numbers
+const Summation=num1 + num2;
+//display the sum
+
+console.log(`The sum of ${num1} and ${num2} is : ${Summation}`)
+
